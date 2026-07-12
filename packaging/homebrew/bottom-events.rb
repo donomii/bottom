d0ad8421bdb373583a56525e5e170a28ade0c506
Your Name <you@example.com>
2026-07-11 23:14:42 -0700

    build: update packages for v0.1.2

--- packaging/homebrew/bottom-events.rb
+++ packaging/homebrew/bottom-events.rb
@@ -1,28 +1,28 @@
 class BottomEvents < Formula
   desc "Read-only process lifecycle flight recorder"
   homepage "https://github.com/donomii/bottom"
-  version "0.1.1"
+  version "0.1.2"
   license "GPL-3.0-only"
 
   on_macos do
     on_arm do
-      url "https://github.com/donomii/bottom/releases/download/v0.1.1/bottom-events_0.1.1_darwin_arm64.tar.gz"
-      sha256 "9b98523a26ff80d519483835029e03fd923fba39ea5ea256c3e8c136e14d3c59"
+      url "https://github.com/donomii/bottom/releases/download/v0.1.2/bottom-events_0.1.2_darwin_arm64.tar.gz"
+      sha256 "6d429c61c626f34cbf015fb9c68e62ef220414345dd8b419edc832d5606fa773"
     end
     on_intel do
-      url "https://github.com/donomii/bottom/releases/download/v0.1.1/bottom-events_0.1.1_darwin_amd64.tar.gz"
-      sha256 "7ad73ebe403e99dd3ae74b0a26006c35f2a36abfaf1d36c447fb557fa765ff6d"
+      url "https://github.com/donomii/bottom/releases/download/v0.1.2/bottom-events_0.1.2_darwin_amd64.tar.gz"
+      sha256 "369aeddf7c748ff62420aac6d08a04abab04ab50339a7516de5de928a488b032"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/donomii/bottom/releases/download/v0.1.1/bottom-events_0.1.1_linux_arm64.tar.gz"
-      sha256 "fb16b22525ebccdd455fed1acfd2a31987215f9915a218d1ca947f0a430d394d"
+      url "https://github.com/donomii/bottom/releases/download/v0.1.2/bottom-events_0.1.2_linux_arm64.tar.gz"
+      sha256 "dfef5329c7f3f150e8fac87d145a2fa192a30565a0f6ae510548c972ca80ce10"
     end
     on_intel do
-      url "https://github.com/donomii/bottom/releases/download/v0.1.1/bottom-events_0.1.1_linux_amd64.tar.gz"
-      sha256 "ab567e8abbb3c47823ad1d00624de00217b4b0877a4a988f1bce34e31de443e8"
+      url "https://github.com/donomii/bottom/releases/download/v0.1.2/bottom-events_0.1.2_linux_amd64.tar.gz"
+      sha256 "153c68774c3cb459d8c7c0b4842b66e007a537a6d7ac680484068285351f2ee4"
     end
   end
 
@@ -32,6 +32,6 @@ class BottomEvents < Formula
   end
 
   test do
-    assert_match "bottom 0.1.1", shell_output("#{bin}/bottom version")
+    assert_match(/bottom v?0\.1\.2/, shell_output("#{bin}/bottom version"))
   end
 end
